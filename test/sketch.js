528bcfc1d5558933b4487fb530faf486cc52aaee
何依朗 <yilanghe@sohu-inc.com>
2017-12-08 18:04:30 +0800

    1208

--- test/sketch.js
+++ test/sketch.js
@@ -12,26 +12,14 @@
 // 	ellipse(mouseX,mouseY,50,50);
 // 	return false;
 // }
-
-var value = 0;
-var value2 = 0;
-
-function draw() {
-  fill(value);
-  rect(25, 25, 50, 50);
-  fill(value2);
-  rect(50, 50, 50, 50);
+function setup(){
+  createCanvas(windowWidth, 400, WEBGL);
 }
 
-function deviceShaken() {
-  value = value + 5;
-  if (value > 255) {
-    value = 0;
-  }
-}
-function deviceMoved() {
-  value2 = value2 + 5;
-  if (value2 > 255) {
-    value2 = 0;
-  }
+function draw(){
+  background(200);
+  rotateZ(radians(rotationZ));
+  rotateX(radians(rotationX));
+  rotateY(radians(rotationY));
+  box(100, 100, 100);
 }
