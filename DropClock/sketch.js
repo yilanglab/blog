fc73dbeabc5e9951e7e2c456213dd79c7c80a961
yilang <236250373@qq.com>
2019-03-26 18:28:32 +0800

    0326

--- DropClock/sketch.js
+++ DropClock/sketch.js
@@ -12,59 +12,59 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(0,0,255);
   translate(width/2,height/2)
   
   noFill();
   
   if(s != second()){
-		let p1 = new Ripple();
-		ripplesH.push(p1);
+    let p1 = new Ripple();
+    ripplesH.push(p1);
     
     let p2 = new Ripple();
-		ripplesM.push(p2);
+    ripplesM.push(p2);
     
     let p3 = new Ripple();
-		ripplesS.push(p3);
+    ripplesS.push(p3);
     
-		s = second()
-	}
+    s = second()
+  }
   
   // =========================== HOUR
   strokeWeight(3);
   for (let i = ripplesH.length-1; i>=0; i--){
-		ripplesH[i].showH();
-		ripplesH[i].updateH();
-		if (ripplesH[i].finishedH()){
-			ripplesH.splice(i,1);
-		}
-	}
+    ripplesH[i].showH();
+    ripplesH[i].updateH();
+    if (ripplesH[i].finishedH()){
+      ripplesH.splice(i,1);
+    }
+  }
   // =========================== MINUTE
   strokeWeight(2);
   for (let i = ripplesM.length-1; i>=0; i--){
-		ripplesM[i].showM();
-		ripplesM[i].updateM();
-		if (ripplesM[i].finishedM()){
-			ripplesM.splice(i,1);
-		}
-	}
+    ripplesM[i].showM();
+    ripplesM[i].updateM();
+    if (ripplesM[i].finishedM()){
+      ripplesM.splice(i,1);
+    }
+  }
   // =========================== SECOND
   strokeWeight(1);
   for (let i = ripplesS.length-1; i>=0; i--){
-		ripplesS[i].showS();
-		ripplesS[i].updateS();
-		if (ripplesS[i].finishedS()){
-			ripplesS.splice(i,1);
-		}
-	}
+    ripplesS[i].showS();
+    ripplesS[i].updateS();
+    if (ripplesS[i].finishedS()){
+      ripplesS.splice(i,1);
+    }
+  }
   
   push();
   noFill();
   strokeWeight(300);
-  stroke(255);
+  stroke(0,0,255);
   ellipse(0,0,600,600);
   strokeWeight(1);
-  stroke(0,20);
+  stroke(255,100);
   ellipse(0,0,300,300);
   pop();
 }
@@ -92,46 +92,46 @@ class Ripple {
   }
   // =========================== MINUTE
   finishedM(){
-		return this.alphaM <0;
-	}
+    return this.alphaM <0;
+  }
   
   updateM(){
-		// this.x-=this.vx;
-		// this.y-=this.vy;
-		this.alphaM -= 1;
+    // this.x-=this.vx;
+    // this.y-=this.vy;
+    this.alphaM -= 1;
     this.radiusM += 1;
-	}
+  }
   
   showM(){
-    stroke(0,0,255,this.alphaM);
+    stroke(255,this.alphaM);
     ellipse(this.xM,this.yM,this.radiusM,this.radiusM);
   }
   // =========================== HOUR
   finishedH(){
-		return this.alphaH <0;
-	}
+    return this.alphaH <0;
+  }
   
   updateH(){
-		this.alphaH -= 0.5;
+    this.alphaH -= 0.5;
     this.radiusH += 1;
-	}
+  }
   
   showH(){
-    stroke(0,0,255,this.alphaH);
+    stroke(255,this.alphaH);
     ellipse(this.xH,this.yH,this.radiusH,this.radiusH);
   }
   // =========================== SECOND
   finishedS(){
-		return this.alphaS <0;
-	}
+    return this.alphaS <0;
+  }
   
   updateS(){
-		this.alphaS -= 2;
+    this.alphaS -= 2;
     this.radiusS += 1;
-	}
+  }
   
   showS(){
-    stroke(0,0,255,this.alphaS);
+    stroke(255,this.alphaS);
     ellipse(this.xS,this.yS,this.radiusS,this.radiusS);
     
   }
