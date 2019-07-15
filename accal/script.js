ee69a8c0101916de74639e305632900d6f0b3320
何依朗 <yilanghe@sohu-inc.com>
2019-07-15 22:59:36 +0800

    no message

---
+++ accal/script.js
@@ -0,0 +1,34 @@
+﻿function CCol(){
+	var buttonAC = document.getElementById("butt");
+	buttonAC.style.background="black";
+}
+
+function CCol2(){
+	var buttonAC = document.getElementById("butt");
+	buttonAC.style.background="pink";
+}
+
+// function setWH(){
+// 	var bgW = window.innerWidth+"px";
+// 	var bgH = window.offsetheight+"px";
+// 	var backg = document.getElementById("bg");
+// 	bg.style.width=bgW;
+// 	bg.style.height=bgH;
+// }
+
+document.getElementById("butt").addEventListener("touchstart", touch);
+document.getElementById("butt").addEventListener('touchend', touch);
+function touch(event) {
+    /**兼用 IE 浏览器*/
+    var event = event || window.event;
+    var obj = document.getElementById("butt");
+    switch (event.type) {
+        case "touchstart":
+            obj.src = "buttonDown.png";
+            break;
+        case "touchend":
+            obj.src = "buttonUp.png";
+            break;
+        
+    }
+}
