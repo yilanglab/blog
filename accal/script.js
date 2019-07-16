48923e982e51dd343278f95a9f375afaca7bc8af
何依朗 <yilanghe@sohu-inc.com>
2019-07-16 13:18:43 +0800

    no message

--- accal/script.js
+++ accal/script.js
@@ -6,24 +6,24 @@
 // }
 // setWH();
 
-function play(){
-	document.getElementById("audio").pause();
-	document.getElementById("audio").currentTime = 0;
-    document.getElementById("audio").play();
-}
+var sound = new Howl({
+    src: ['AC.mp3']
+  });
 
 document.getElementById("butt").addEventListener("touchstart", touch);
+document.getElementById("butt").addEventListener('touchmove', touch);
 document.getElementById("butt").addEventListener('touchend', touch);
 function touch(event) {
     /**兼用 IE 浏览器*/
     var event = event || window.event;
     var acbutton = document.getElementById("butt");
-    var num = document.getElementById("num");
 
     switch (event.type) {
         case "touchstart":
-        	play();
+        	sound.stop();
+            sound.play();
             acbutton.src = "buttonDown.png";
+            var num = document.getElementById("num");
             num.style.display = "none";
     		var timer = setTimeout(function(){
     			var num = document.getElementById("num");
@@ -33,6 +33,8 @@ function touch(event) {
         case "touchend":
             acbutton.src = "buttonUp.png";
             break;
-        
+        case "touchmove":
+            acbutton.src = "buttonDown.png";
+            break;
     }
 }
